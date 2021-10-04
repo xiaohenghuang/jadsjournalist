65ddab5697425546e75e27a2c197126841d0fd5c
xiaoheng <huangxiaohengzju@gmail.com>
2021-10-04 16:44:46 +0200

    xiaoheng: added some preparation.

---
+++ draft/xiaoheng/Google/jadsjournalist/jadsjournalist/spiders/code.py
@@ -0,0 +1,24 @@
+import scrapy
+#from jadsjournalist.items import FundrazrItem
+from datetime import datetime
+import re
+
+class Headlines(scrapy.Spider):
+    name = "journalists"
+
+    # my urls
+    start_urls = ["https://news.google.com/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen", # US
+                  "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", # world
+                  "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", # Business
+                  "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", # Technology
+                  "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", # entertainments
+                  "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", # sports
+                  "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", #science
+                  "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen" # health
+                  ]
+
+    def parse(self, start_urls):
+        for url in start_urls:
+            #url = "https:" + href.extract()
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+    def parse_dir_contents(self, response):
